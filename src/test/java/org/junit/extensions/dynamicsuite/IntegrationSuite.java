783377ec82da2e2e3cb814659dfa8930e9e9c216
Christof Schoell <c.schoell@gmx.net>
2011-07-08 15:53:59 +0200

    Add Apache License to Source Files

--- src/test/java/org/junit/extensions/dynamicsuite/IntegrationSuite.java
+++ src/test/java/org/junit/extensions/dynamicsuite/IntegrationSuite.java
@@ -4,9 +4,19 @@ import org.junit.extensions.dynamicsuite.filter.DefaultFilter;
 import org.junit.runner.RunWith;
 
 /**
- * User: Chris
- * Date: 01.07.11
- * Time: 09:43
+ * Copyright 2011 Christof Schoell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 @RunWith(DynamicSuite.class)
 @DirectoryFilter(IntegrationSuite.class)
